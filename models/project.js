86f1fade7c16f174afd0ae14daf07384efabbd60
Jonathan <jhfernan@mail.usf.edu>
2017-08-30 15:56:50 -0400

    Added modal for adding project

--- models/project.js
+++ models/project.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 
 const projectSchema = mongoose.Schema({
 	name: { type: String, required: true, lowercase: true, trim: true },
+	notes: String,
 	createdAt: { type: Date, default: Date.now },
 	times: [{
 		start: Date,
